2039381a93be09c80cf1ca528da9ca50127253e8
nadavoved <nadavove@gmail.com>
2023-01-09 08:29:28 +0200

    load redone

--- load.py
+++ load.py
@@ -1,65 +1,94 @@
-import pandas as pd
-from pandas import Int16Dtype, BooleanDtype
+import os
 from inspect import stack
+
+import pandas as pd
+
+
 
 """Module to allow only one csv DataFrame to exist at 
 once and optimize its memory requirement.
 """
 
-TYPE_MAP = {'Year': Int16Dtype(),
-            'Month': Int16Dtype(),
-            'DayofMonth': Int16Dtype(),
-            'DayOfWeek': Int16Dtype(),
-            'Cancelled': BooleanDtype(),
-            'Diverted': BooleanDtype(),
-            'UniqueCarrier': 'object',
-            'Origin': 'object',
-            'Dest': 'object',
-            'FlightNum': 'string',
-            'ActualElapsedTime': Int16Dtype(),
-            'AirTime': Int16Dtype(),
-            'Distance': Int16Dtype()}
+TYPE_MAP = {'Year': 'category',
+            'Month': 'category',
+            'DayofMonth': 'category',
+            'DayOfWeek': 'category',
+            'Cancelled': pd.BooleanDtype(),
+            'ArrDelay': 'float32',
+            'DepDelay': 'float32',
+            'Diverted': pd.BooleanDtype(),
+            'Origin': 'category',
+            'Dest': 'category',
+            'UniqueCarrier': 'category',
+            'ActualElapsedTime': 'float32',
+            'AirTime': 'float32',
+            'Distance': 'float32'}
 
-FINAL_TYPES = {item: 'category' for item in ['Year', 'Month', 'DayofMonth',
-                                             'DayOfWeek', 'UniqueCarrier', 'Origin', 'Dest']}
+FINAL_TYPES = {item: 'category' for item in TYPE_MAP
+               if TYPE_MAP[item] == 'category'}
+COLS = ['UniqueCarrier'] + [label for label in TYPE_MAP.keys()
+                            if label != 'UniqueCarrier']
+
+
+def data_to_feather():
+    def csv_to_feather(fp):
+        temp = pd.read_csv(fp, encoding='cp1252',
+                           usecols=COLS, dtype=TYPE_MAP)
+        os.remove(fp)
+        new_path = f"{fp.rstrip('csv')}feather"
+        temp.to_feather(path=new_path)
+        print(f'converted {fp} to {new_path}.')
+
+    for i in range(1987, 2009):
+        csv_to_feather(f'data/{i}.csv')
 
 
 def wipe_frames():
-    """Delete all DataFrames"""
-    g = stack()[2][0].f_globals
+    """Delete all DataFrames from memory."""
+    g = stack()[1][0].f_globals
     all_variables = [item for item in g]
     for var in all_variables:
         if isinstance(g[var], pd.DataFrame):
             del g[var]
 
 
-def get_years(*args, first_year: int = None, last_year: int = None, wipe=True) -> pd.DataFrame:
-    """Read specific years from the relevant csv's.
-    Using only required columns and optimal dtypes.
-    Allow a maximum of 5 years.
+def get_years(*args: int, year_range: tuple[int, int] = None, sample_size=1000) -> pd.DataFrame:
+    """Sample specific years from the relevant csv's.
+    Use only required columns and optimal dtypes.
+    Allow a maximum of 10 years.
     #NOTE - even then its execution time is relatively long.
-    :param first_year: (int) the first year to load data from.
-    :param last_year: (int) the last year to load data from.
-    :param wipe: (bool) to wipe previous frames or not.
+    :param args: (int) individual years to load data from.
+    :param year_range: tuple[int, int] the first and last years to load data from.
+    :param sample_size: (int) sample size to draw from each year.
     """
-    if wipe:
-        wipe_frames()
 
     def read_year(yr):
-        return pd.read_csv(f'data/{yr}.csv', encoding='cp1252',
-                           usecols=TYPE_MAP.keys(), dtype=TYPE_MAP)
+        return pd.read_feather(f'data/{yr}.feather',
+                               columns=COLS).sample(sample_size)
 
     def verify_rng(x: range | tuple):
-        if len(list(x)) > 5:
-            raise OverflowError('should not include more than 5 years.')
+        if len(list(x)) > 10:
+            raise OverflowError('should not include more than 10 years.')
         if any(y not in range(1987, 2009) for y in x):
             raise ValueError('input not in range.')
         return x
 
-    if args:
-        rng = verify_rng(args)
-    else:
-        rng = verify_rng(range(first_year, last_year + 1))
+    match (bool(args), bool(year_range)):
+        case (True, False):
+            rng = verify_rng(args)
+        case (False, True):
+            if len(year_range) != 2:
+                raise ValueError('year_range should include start and end years.')
+            rng = verify_rng(range(year_range[0], year_range[1] + 1))
+        case (False, False):
+            raise ValueError('No years are given.')
+        case _:
+            raise ValueError('Can\'t accept both individual years and range')
     year_slice = [read_year(yr) for yr in rng]
-    frame = pd.concat(objs=year_slice, ignore_index=True)
-    return frame.astype(dtype=FINAL_TYPES).rename({'DayofMonth': 'DayOfMonth'})
+    frame = pd.concat(objs=year_slice, ignore_index=True).sample(sample_size)
+    return frame.astype(dtype=FINAL_TYPES).reindex(columns=COLS).\
+        rename(columns={'DayofMonth': 'DayOfMonth'})
+
+if __name__ == '__main__':
+    data_to_feather()
+
